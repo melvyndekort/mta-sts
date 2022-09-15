675a03d0508385861ed5f2257ccaa72207345882
Melvyn de Kort <m.de.kort@portbase.com>
2022-09-15 11:51:16 +0200

    Use correct state file

--- terraform/state.tf
+++ terraform/state.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket     = "mdekort.tfstate"
-    key        = "startpage.tfstate"
+    key        = "mta-sts.tfstate"
     region     = "eu-west-1"
     encrypt    = "true"
     kms_key_id = "arn:aws:kms:eu-west-1:075673041815:alias/generic"
