f230e11aa4b863d093b09b9c61287c0fec835ee1
Melvyn de Kort <melvyn@mdekort.nl>
2025-10-03 21:51:35 +0200

    Remove cloudsetup remote state

--- terraform/remote_state.tf
+++ terraform/remote_state.tf
@@ -1,13 +1,3 @@
-data "terraform_remote_state" "cloudsetup" {
-  backend = "s3"
-
-  config = {
-    bucket = "mdekort.tfstate"
-    key    = "cloudsetup.tfstate"
-    region = "eu-west-1"
-  }
-}
-
 data "terraform_remote_state" "tf_cloudflare" {
   backend = "s3"
 
